c59aab8393ab3e83d2a6c6c7c88c7303d9c2f389
kingsley ijomah <kingsley.ijomah@gmail.com>
2021-09-08 11:09:41 +0100

    Added expense model with migrations

--- expense_api/models.py
+++ expense_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Expense(models.Model):
+    amount = models.FloatField()
+    merchant = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.merchant
--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", include(("expense_api.urls", "expense_api"), namespace="expense_api")),
+    path("admin/", admin.site.urls),
 ]
